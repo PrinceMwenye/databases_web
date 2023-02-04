945eda66e1cf4b440270a7ccdc9a6bd80d439040
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-02-04 15:26:39 -0800

    Add boiler

--- index.js
+++ index.js
@@ -3,11 +3,76 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const path = require('path');
+
 
 app.get('/', (req,res) => {
-    res.send("<h1>Hello Worlddddddd!</h1>");
+    res.send("<h1>Hello World!</h1>");
 });
+
+app.get('/signup', (req, res) => {
+    var missingEmail = req.query.missing;
+    var html = res.sendFile(__dirname + "/signup.html");
+    if (missingEmail) {
+        html += "<br> email is required";
+    }
+    res.send(html);
+});
+
+
+
+
+
+
+app.get('/contact', (req,res) => {
+    var missingEmail = req.query.missing;
+    var html = `
+        email address:
+        <form action='/submitEmail' method='post'>
+            <input name='email' type='text' placeholder='email'>
+            <button>Submit</button>
+        </form>
+    `;
+    if (missingEmail) {
+        html += "<br> email is required";
+    }
+    res.send(html);
+});
+
+app.post('/submitEmail', (req,res) => {
+    var email = req.body.email;
+    if (!email) {
+        res.redirect('/contact?missing=1');
+    }
+    else {
+        res.send("Thanks for subscribing with your email: "+email);
+    }
+});
+
+
+// app.get('/cat/:id', (req,res) => {
+
+//     var cat = req.params.id;
+
+//     if (cat == 1) {
+//         res.send("Fluffy: <img src='/fluffy.gif' style='width:250px;'>");
+//     }
+//     else if (cat == 2) {
+//         res.send("Socks: <img src='/socks.gif' style='width:250px;'>");
+//     }
+//     else {
+//         res.send("Invalid cat id: "+cat);
+//     }
+// });
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req,res) => {
+	res.status(404);
+	res.send("Page not found - 404");
+})
 
 app.listen(port, () => {
 	console.log("Node application listening on port "+port);
-})
+}); 
