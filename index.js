5cec1f8d090fac1c037529cd2f0c46c8b184b706
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-02-06 08:33:36 -0800

    install express session

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
