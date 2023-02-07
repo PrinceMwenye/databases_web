05934b4f09b7981097fab58573ff5637ef54deae
Prince H Chabveka <pchabveka@my.bcit.ca>
2023-02-06 20:27:04 -0800

    change expire time to one hour

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+const expireTime = 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
 
 
 //Users and Passwords (in memory 'database')
